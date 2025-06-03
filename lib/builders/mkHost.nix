f7af6daa2d2c36b1de697e8ffc84553276c3619c
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-06-03 14:54:39 -0700

    builders/mkHost: deprecate options/ import

--- lib/builders/mkHost.nix
+++ lib/builders/mkHost.nix
@@ -54,17 +54,8 @@ in
             (map (n: (moduleDir + /desktops + /${n} + /module.nix)) desktops)
             (map (n: (moduleDir + /profiles + /${n} + /module.nix)) profiles)
 
-            # Import desktop and profile options modules
-            # In theory, this shouldn't cause problems, so long as each host
-            # import the desktop/profiles that they want to make available & configure
-            # I could restructure my config to simplify the function, but I want to make
-            # it easier to revert.
-            # (map (n: (moduleDir + /options/desktops + /${n} + ".nix")) desktops)
-            #(map (n: (moduleDir + /options/profiles + /${n} + ".nix")) profiles)
-
             # All hosts import the common modules
             (listNixRecursive (moduleDir + /common))
-            (listNixRecursive (moduleDir + /options))
 
             # Import host modules
             (listNixRecursive (hostDir + /${hostName}))
